14af3a7577668c0a946d90c5f3ae755cec7b7480
Adam <adam.chunjung@gmail.com>
2022-05-16 18:36:54 +0800

    change img url

--- src/DUMMY_DATA.js
+++ src/DUMMY_DATA.js
@@ -2,115 +2,115 @@ export const DUMMY_DATA = [
   {
     name: '愚者 The Fool',
     text: '沒有按計劃進行，唯一可以做的就是開心地笑。有些事情使得您嘗試越多，成功就越少。情況可能有點滑。機會從您手中溜走了，您可能會迷失在似乎如此簡單的道路上。但是，仍然不用擔心結果。',
-    image: 'https://myscith.com/wp-content/uploads/2021/03/tarot-fool.JPG',
+    image: 'https://myscith.com/wp-content/uploads/2021/03/tarot-fool.jpg',
   },
   {
     name: '死神 Death',
     text: '您的周圍發生險惡（儘管不一定是死亡）的事情，它可能或多或少地困擾著您，但可能並非主要發生在您身上。儘管如此，仍有理由要小心。',
-    image: 'https://myscith.com/wp-content/uploads/2021/03/tarot-death.JPG',
+    image: 'https://myscith.com/wp-content/uploads/2021/03/tarot-death.jpg',
   },
   {
     name: '戀人 The Lovers',
     text: '您愛上一個可能愛上您的人的機會。簡單地說，就是一次相愛的機會。確實有很多機會。該卡最有可能表明它會發生，無論它是否在您的未來計劃中。',
-    image: 'https://myscith.com/wp-content/uploads/2021/03/tarot-lovers.JPG',
+    image: 'https://myscith.com/wp-content/uploads/2021/03/tarot-lovers.jpg',
   },
   {
     name: '戰車 The Chariot',
     text: '事情的結果是最幸運的，但是您必須考慮可能帶來的長期後果。嫉妒他人絕對是其中之一。',
-    image: 'https://myscith.com/wp-content/uploads/2021/03/tarot-chariot.JPG',
+    image: 'https://myscith.com/wp-content/uploads/2021/03/tarot-chariot.jpg',
   },
   {
     name: '惡魔 The Devil',
     text: '這是一個釋放您的束縛並滿足您的慾望，探索您以前不曾膽識的機會的機會。當然，和魔鬼一樣，這是有代價的。一旦您一時衝動，就準備好付款。魔鬼卡問了一個問題：您是否敢為誘惑而跌倒？如果不是，那麼您是否有能力抵抗它？',
-    image: 'https://myscith.com/wp-content/uploads/2021/03/tarot-devil.JPG',
+    image: 'https://myscith.com/wp-content/uploads/2021/03/tarot-devil.jpg',
   },
   {
     name: '國王 The Emperor',
     text: '您必須接受事情結果不在您的掌握範圍之內。決定權在其他地方，您只需要遵守-否則後果自負。正如他們所說：您不能上位者抗爭。',
-    image: 'https://myscith.com/wp-content/uploads/2021/03/tarot-emperor.JPG',
+    image: 'https://myscith.com/wp-content/uploads/2021/03/tarot-emperor.jpg',
   },
   {
     name: '教皇 The Hierophant',
     text: '這一刻具有重要的象徵意義，它可能導致勝利或貶低。必須重視它的重要性，否則事情不太可能如願以償。不過，您自己的影響很小。涉及超出您能力範圍的事情。除了抱有對最好的希望之外，沒有太多要做的事情了，並確保不要冒犯任何人。',
     image:
-      'https://myscith.com/wp-content/uploads/2021/03/tarot-hierophant.JPG',
+      'https://myscith.com/wp-content/uploads/2021/03/tarot-hierophant.jpg',
   },
   {
     name: '皇后 The Empress',
     text: '需要保持鎮靜並仔細考慮操作的選擇，因為事情要比看起來要復雜得多。而且，它表明即使可以做到，通過溫柔也要比通過蠻力完成更多的工作。搜索並非雙贏，而是尋求雙贏的解決方案。每個人都會為此而稱讚您。',
-    image: 'https://myscith.com/wp-content/uploads/2021/03/tarot-empress.JPG',
+    image: 'https://myscith.com/wp-content/uploads/2021/03/tarot-empress.jpg',
   },
   {
     name: '倒吊人 The Hanged Man',
     text: '目前的事件是需要個人犧牲的。可能是您的，也可能是別人的。問題是誰準備接受它。如果沒有人，將沒有解決方案。如果這樣做，無論是誰，一切都會好起來的-對於做出犧牲的人來說，每個人都可以。生活並不總是公平的。做出犧牲的人的安慰，如果有人這樣做，是個人的滿足感。那是相當的東西。',
-    image: 'https://myscith.com/wp-content/uploads/2021/03/tarot-hangedman.JPG',
+    image: 'https://myscith.com/wp-content/uploads/2021/03/tarot-hangedman.jpg',
   },
   {
     name: '隱者 The Hermit',
     text: '這一刻具有重要的象徵意義，它可能導致勝利或貶低。必須重視它的重要性，否則事情不太可能如願以償。不過，您自己的影響很小。涉及超出您能力範圍的事情。除了抱有對最好的希望之外，沒有太多要做的事情了，並確保不要冒犯任何人。',
-    image: 'https://myscith.com/wp-content/uploads/2021/03/tarot-hermit.JPG',
+    image: 'https://myscith.com/wp-content/uploads/2021/03/tarot-hermit.jpg',
   },
   {
     name: '女祭司 The High Priestess',
     text: '需要解決一個複雜的問題，就像一個謎一樣，您必須考慮一下才能看到它。其他人甚至可能沒有意識到有障礙，但是即使不被承認和處理，失敗也是肯定的-儘管失敗可能是一個完全的意外，而且沒人知道為什麼。',
     image:
-      'https://myscith.com/wp-content/uploads/2021/03/tarot-highpriestess.JPG',
+      'https://myscith.com/wp-content/uploads/2021/03/tarot-highpriestess.jpg',
   },
   {
     name: '審判 Judgement',
     text: '意味著最終裁決將到來，並且任何人都無法對此做任何事情。解決任何情況的解決方案，幾乎都與所涉人員的行動無關。就像沒有其他選擇一樣，它只是發生了。反對是徒勞的。',
-    image: 'https://myscith.com/wp-content/uploads/2021/03/tarot-judgement.JPG',
+    image: 'https://myscith.com/wp-content/uploads/2021/03/tarot-judgement.jpg',
   },
   {
     name: '正義 Justice',
     text: '意味著您將面臨某種審判。它不一定在法庭上，但會做出判決，它將對您產生影響。可能不管您做什麼，您對判斷的影響都很小。因此，您會做好準備以準備任何結果。',
-    image: 'https://myscith.com/wp-content/uploads/2021/03/tarot-justice.JPG',
+    image: 'https://myscith.com/wp-content/uploads/2021/03/tarot-justice.jpg',
   },
   {
     name: '月亮 The Moon',
     text: '您的渴望對您顯而易見，而發生的事情會增加它。它可能會在您內部造成情緒混亂，但您還是歡迎它-儘管有些擔心。至少，您會想起自己的情感需求以及設法滿足這些需求的重要性。',
-    image: 'https://myscith.com/wp-content/uploads/2021/03/tarot-moon.JPG',
+    image: 'https://myscith.com/wp-content/uploads/2021/03/tarot-moon.jpg',
   },
   {
     name: '魔術師 The Magician',
     text: '表明存在一種解決方案，似乎是靠魔術來解決的。莫名其妙。沒有合乎邏輯的方法可以解決問題，但是荒謬的方法可能會解決。完全不相關的事情可能被證明是唯一相關的措施。即使看起來確實可行，它也不是完全值得信賴的，但它可能是唯一剩下的選擇。',
-    image: 'https://myscith.com/wp-content/uploads/2021/03/The-Magician-1.JPG',
+    image: 'https://myscith.com/wp-content/uploads/2021/03/The-Magician-1.jpg',
   },
   {
     name: '星星 The Star',
     text: '是時候事情該停頓了，以便您有時間重新考慮，應該這樣做。目標受到質疑。以前堅持的價值觀也是如此。您應該坐下來沉思，直到您對全新的觀點持開放態度。',
-    image: 'https://myscith.com/wp-content/uploads/2021/03/tarot-star.JPG',
+    image: 'https://myscith.com/wp-content/uploads/2021/03/tarot-star.jpg',
   },
   {
     name: '力量 Strength',
     text: '勝利似乎是最不可能發生的事件。就像人類大多數事物一樣，力量在腦海中。當前的問題需要真正的力量，而不僅僅是竭盡全力。找到您的能力的順暢應用。',
-    image: 'https://myscith.com/wp-content/uploads/2021/03/tarot-strength.JPG',
+    image: 'https://myscith.com/wp-content/uploads/2021/03/tarot-strength.jpg',
   },
   {
     name: '太陽 The Sun',
     text: '千載難逢的事情有機會發生，您可以因此而繁榮昌盛。但是，仍然要限制自己的貪婪，考慮一下您真正需要的東西和貪吃的東西。好運與壞運一樣難處理。',
-    image: 'https://myscith.com/wp-content/uploads/2021/03/tarot-sun.JPG',
+    image: 'https://myscith.com/wp-content/uploads/2021/03/tarot-sun.jpg',
   },
   {
     name: '節制 Temperance',
     text: '建議格外小心，因為最終結果不是第一次出現。如果您有耐心等待，您的回報將是甜蜜而持久的。如果您不這樣做，那麼您根本不可能完成很多事情。您甚至可能比一開始面臨的麻煩更大。該卡還可以簡單地暗示您有一段閒置時間，那裡什麼都沒發生。',
     image:
-      'https://myscith.com/wp-content/uploads/2021/03/tarot-temperance.JPG',
+      'https://myscith.com/wp-content/uploads/2021/03/tarot-temperance.jpg',
   },
   {
     name: '高塔 The Tower',
     text: '意味一個雄心勃勃的項目的不幸結局。失敗。您可能瞄準了太高或舉起了太重的重量。它不會持續下去。如果您堅持雄心勃勃的目標，則必須從一開始就做好準備，並且將克服其他障礙。',
-    image: 'https://myscith.com/wp-content/uploads/2021/03/tarot-tower.JPG',
+    image: 'https://myscith.com/wp-content/uploads/2021/03/tarot-tower.jpg',
   },
   {
     name: '命運之輪 The Wheel of Fortune',
     text: '意味著有關當前問題的未來是隱藏的。什麼都可能發生。很難為任何事情做準備，但是至少可以為一個驚喜做準備。最有可能發生的事情就是您認為極不可能發生的事情。',
     image:
-      'https://myscith.com/wp-content/uploads/2021/03/tarot-wheeloffortune.JPG',
+      'https://myscith.com/wp-content/uploads/2021/03/tarot-wheeloffortune.jpg',
   },
   {
     name: '世界 The World',
     text: '事情為您帶來了超出您最初預期的許多可能性。但是，請謹慎選擇。總有一張要付出的代價。您從這次機會中獲得的收益可能會大大超出代價，也可能不會。因此，在涉足事物之前，請仔細考慮可能產生的後果。',
-    image: 'https://myscith.com/wp-content/uploads/2021/03/tarot-world.JPG',
+    image: 'https://myscith.com/wp-content/uploads/2021/03/tarot-world.jpg',
   },
 ];
